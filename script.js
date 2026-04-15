60eeaee049a56f59d6f1cc2760993bb8979b2b7a
cdvincoy <cdvincoy@up.edu.ph>
2026-04-15 21:43:13 +0800

    Final Pages

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This is the source code for script.js.
+
 const container = document.getElementById("agents-container");
 const searchInput = document.getElementById("search");
 
@@ -9,46 +11,87 @@ async function fetchAgents() {
     const data = await res.json();
 
     allAgents = data.data;
+    console.log(allAgents);
+
+
     displayAgents(allAgents);
   } catch (err) {
     console.error("Fetch error:", err);
   }
 }
-
+/*Displaying Agents*/
 function displayAgents(agents) {
-  container.innerHTML = "";
+    container.innerHTML = "";
+    
+    agents.forEach(agent => {
+        const card = document.createElement("div");
+        card.classList.add("agent-card");
+        
+        card.innerHTML = 
+        `<img src="${agent.displayIcon}" alt="${agent.displayName}">
+        <div class="agent-name">${agent.displayName}</div>`;
 
-  agents.forEach(agent => {
-    const card = document.createElement("div");
-    card.classList.add("agent-card");
+        container.appendChild(card);
+        
+        card.addEventListener("click", () => {
+            const modal = document.getElementById("modal");
+            const modalContent = document.getElementById("modal-content");
+            
+            const abilities = agent.abilities.filter(a => a.displayName).map(a => `
+                <div class="ability-section">
+                ${a.displayIcon ? `<img src="${a.displayIcon}" width="30">` : ""}
+                <div class="ability-name"><strong>${a.displayName}</strong><br></div>
+                <div class="ability-desc">
+                    <small>${a.description ? a.description.replace(/\.\s/g,".<br><br>") : "No descriptipn"}</small>
+                </div>
+                </div>`)
+            .join("");
 
-    const abilities = agent.abilities
-      .filter(a => a.displayName)
-      .map(a => `<li>${a.displayName}</li>`)
-      .join("");
+        const img = agent.fullPortraitV2 || agent.fullPortrait;
+        const iconView = agent.background || "";
 
-    card.innerHTML = `
-      <img src="${agent.displayIcon}" alt="${agent.displayName}">
-      <div class="agent-name">${agent.displayName}</div>
-
-      <div class="agent-details">
-        <div class="agent-role">
-          ${agent.role ? agent.role.displayName : "No Role"}
-        </div>
-        <strong>Abilities:</strong>
-        <ul>${abilities}</ul>
-      </div>
-    `;
-
-    // Toggle details on click
-    card.addEventListener("click", () => {
-      const details = card.querySelector(".agent-details");
-      details.classList.toggle("active");
+        modalContent.innerHTML = 
+            `
+            <div class="agent-modal">
+                <div class="icon-layer" style="background-image: url('${iconView}')"></div>
+                <div class="portrait-layer" style="background-image: url('${img}')"></div>
+                <div class="card-top">
+                    <div class="card-left">
+                        <img src="${agent.displayIcon}" alt="${agent.displayName}">
+                        <div class="agent-name">${agent.displayName}</div>
+                    </div>
+            
+                    <div class="card-right">
+                        <div class="agent-role">
+                            ${agent.role ? agent.role.displayName : "No Role"}
+                        </div>
+                        <div class="agent-role-description">
+                            <p>${agent.role ? agent.role.description : "No description available"}</p>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="card-bottom">
+                    <h2 class="abilities-title">Abilities</h2>
+                    <div class="abilities-container">
+                        ${abilities}
+                    </div>
+                </div>
+            </div>
+            `;
+        modal.classList.remove("hidden");
     });
-
-    container.appendChild(card);
   });
 }
+
+/*For Exiting the Modal View*/
+window.addEventListener("DOMContentLoaded", () => {
+document.getElementById("modal").addEventListener("click", (e) => {
+    if (e.target.id === "modal"){
+        document.getElementById("modal").classList.add("hidden");
+    }
+});
+});
 
 /* Search */
 searchInput.addEventListener("input", () => {
@@ -61,4 +104,5 @@ searchInput.addEventListener("input", () => {
   displayAgents(filtered);
 });
 
+/*Fetching Agents from API*/
 fetchAgents();
